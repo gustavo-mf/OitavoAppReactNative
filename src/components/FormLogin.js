791787eead48c6c38bb18d211c6681f9160cae01
Gustavo-mf <gustavo.mf@hotmail.com.br>
2018-02-01 17:21:04 -0200

    Adicionando arquivos reducer e formlogin

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,24 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Button, TextInput,
   Text,View, TouchableHighlight
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { connect } from 'react-redux';
 
-export default props => (
-	<View style={{ flex: 1, padding: 10 }}>
-		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-			<Text style={{ fontSize: 25 }}>WhatsApp Clone</Text>
-		</View>
-		<View style={{ flex: 2}}>
-			<TextInput style={{ fontSize: 20, height: 45}} placeholder='E-mail' />
-			<TextInput  style={{ fontSize: 20, height: 45}} placeholder='Senha' />
-			<TouchableHighlight onPress={ () => Actions.formCadastro() } >
-				<Text style={{ fontSize: 20 }}>Ainda não tem cadastro? Cadastre-se</Text>
-			</TouchableHighlight>
-		</View>
-		<View style={{ flex: 2}}>
-			<Button title="Acessar" color="#006400" onPress={() => false} />
-		</View>
-	</View>
-);
+const formLogin = props => {
+  console.log(props);
+  return (
+    <View style={{ flex: 1, padding: 10 }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontSize: 25 }}>WhatsApp Clone</Text>
+      </View>
+      <View style={{ flex: 2}}>
+        <TextInput value={props.email} style={{ fontSize: 20, height: 45}} placeholder='E-mail' />
+        <TextInput value={props.senha} style={{ fontSize: 20, height: 45}} placeholder='Senha' />
+        <TouchableHighlight onPress={ () => Actions.formCadastro() } >
+          <Text style={{ fontSize: 20 }}>Ainda não tem cadastro? Cadastre-se</Text>
+        </TouchableHighlight>
+      </View>
+      <View style={{ flex: 2}}>
+        <Button title="Acessar" color="#006400" onPress={() => false} />
+      </View>
+    </View>
+  );
+};
+
+const mapStateToProps = state => ({
+  email: state.AutenticacaoReducer.email,
+  senha: state.AutenticacaoReducer.senha
+});
+
+export default connect(mapStateToProps, null)(formLogin);
---
+++ src/reducers/AutenticacaoReducer.js
@@ -0,0 +1,9 @@
+const INITIAL_STATE = {
+  nome: 'gustavo',
+  email: 'gustavodsdsd',
+  senha: '1234'
+}
+
+export default (state = INITIAL_STATE, acntion) => {
+  return state;
+}
